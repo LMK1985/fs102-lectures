d9f2ed7109909dcab0d6beb0fef823f20f310a4e
LMK1985 <lammankid@gmail.com>
2021-02-27 13:30:09 +0800

    Added more codes

--- Lesson 3/js/script.js
+++ Lesson 3/js/script.js
@@ -60,14 +60,67 @@ do {
 
 // Functions to execute a line of codes upon calling the function "greet();"
 function greet() {
-    console.log("Hello there");
+    console.log("FS102");
 }
 greet(); //execute the code inside function greet()
 
 // Function with variables
 function greet2(time, name) {
-    var time = "Morning";
-    var name = "MK";
     console.log(`Good ${time}, ${name}`);
 }
-greet2();
+greet2("Afternoon", "MK");
+
+// Function EX
+function average(x, y) {
+    return (x + y) / 2;
+}
+average(10, -4);
+average(2.5, 8);
+console.log(average(2.5, 8)); // Can console log the whole function to get the return value to show
+
+/*Function prompting for name input. Prompt is same as python input.
+function myFunction() {
+    var person = prompt("Please enter your name", "My Name");
+    if (person != null) {
+      console.log("Hello " + person + "! How are you today?");
+    }
+  }
+  myFunction(); comment out due to annoyance*/
+
+  //Functions EX 2
+  function add(c, d) {
+      console.log((c + d));
+  }
+  add(1, 9);
+
+  /*Functions EX 3
+  function addinput() {
+      var n1 = prompt("Please enter number 1");
+      var n2 = prompt("Please enter number 2");
+      var added = (n1 + n2);
+      if (added != null) {
+          console.log("The sum of the 2 numbers is: " + added);
+      }
+  }
+  addinput(); comment out due to prompt annoyance*/
+
+  //Class Activity
+function calculator() {
+    var n1 = parseInt(prompt("Please enter number 1"));
+    var operator = prompt("Please enter the operation method");
+    var n2 = parseInt(prompt("Please enter number 2"));
+
+    if (operator === "+") {
+        var result = parseInt(n1 + n2);
+    } else if (operator === "-") {
+        var result = parseInt(n1 - n2);
+    } else if (operator === "*") {
+        var result = parseInt(n1 * n2);
+    } else if (operator === "/") {
+        var result = parseInt(n1 / n2);
+    } else {
+        var result = "Please enter a valid operator";
+    }
+    console.log(result);
+}
+calculator();
