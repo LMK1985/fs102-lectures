b750275cee305b029c5a28a58eef6d99a1c58e15
LMK1985 <lammankid@gmail.com>
2021-02-27 10:30:29 +0800

    Added lesson3

---
+++ Lesson 3/js/script.js
@@ -0,0 +1,14 @@
+//Loops Ex 3
+var i;
+for (i = 9; i >= 0; i--) {
+    console.log(i);
+}
+
+//Loops Ex 2 with if else
+for (i = 0; i <= 20; i++) {
+    if (i % 2) {
+        console.log(i + " is even");
+    } else {
+        console.log(i + " is odd");
+    }
+}
